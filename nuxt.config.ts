0b029cb25384f8501942882d476779b0190a7198
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-11-05 18:06:23 -0600

    Adding sharing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      meta: [
+        // Open Graph / Facebook
+        { property: 'og:image', content: '/sharing.jpg' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:type', content: 'image/jpeg' },
+        { property: 'og:type', content: 'website' },
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:image', content: '/sharing.jpg' },
+      ]
+    }
+  },
   nitro: {
     // Configure for Netlify serverless
     preset: 'netlify',
